dc1afb899164475a3237c0f6535275f30b8a0edb
yingfan-bai <yingfan.bai@smus.ca>
2024-05-09 14:48:36 -0700

    Update Howards_eCafe.java

--- src/Howards_eCafe.java
+++ src/Howards_eCafe.java
@@ -1,3 +1,11 @@
+import java.awt.Color; //Imports for this program
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Howards_eCafe {
 
