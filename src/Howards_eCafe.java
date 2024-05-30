1b61103815e8badb4805ea4093d547b3f3657170
Finlay Hlannon <79950262+finlayhlannon@users.noreply.github.com>
2024-05-30 15:03:54 -0700

    button resizing

--- src/Howards_eCafe.java
+++ src/Howards_eCafe.java
@@ -297,13 +297,18 @@ public class Howards_eCafe extends JFrame implements ActionListener {
         c.add(instructionsButton);
 
         
+     // Play again button setup
         ImageIcon playagainImageIcon = new ImageIcon("src/imgs/playagain.png");
         Image playagainImage = playagainImageIcon.getImage();
-        Image playagainNewimg = playagainImage.getScaledInstance(100, 40,  java.awt.Image.SCALE_SMOOTH);
+        int playagainButtonWidth = 100;
+        int playagainButtonHeight = 40;
+        Image playagainNewimg = playagainImage.getScaledInstance(playagainButtonWidth, playagainButtonHeight, java.awt.Image.SCALE_SMOOTH);
         Icon playagainIcon = new ImageIcon(playagainNewimg);
+        
         playagainButton = new JButton(playagainIcon);
         playagainButton.addActionListener(this);
         playagainButton.setVisible(false);
+        playagainButton.setPreferredSize(new Dimension(playagainButtonWidth, playagainButtonHeight));
         c.add(playagainButton);
             
         ImageIcon loseImageIcon = new ImageIcon("src/imgs/losescreen.png");
@@ -324,11 +329,18 @@ public class Howards_eCafe extends JFrame implements ActionListener {
         
         ImageIcon backbuttonImageIcon = new ImageIcon("src/imgs/backbutton.png");
         Image backbuttonImage = backbuttonImageIcon.getImage();
-        Image backbuttonNewimg = backbuttonImage.getScaledInstance(30, 40,  java.awt.Image.SCALE_SMOOTH);
+        Image backbuttonNewimg = backbuttonImage.getScaledInstance(30, 40, java.awt.Image.SCALE_SMOOTH);
         Icon backbuttonIcon = new ImageIcon(backbuttonNewimg);
+        
+        // Create the button with the icon
         menuButton = new JButton(backbuttonIcon);
         menuButton.addActionListener(this);
         menuButton.setVisible(false);
+        
+        // Set the button's preferred size to match the image dimensions
+        menuButton.setPreferredSize(new Dimension(30, 40));
+        
+        // Add the button to the container
         c.add(menuButton);
         
         ImageIcon recipeImageIcon = new ImageIcon("src/imgs/recipes.png");
